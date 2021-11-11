e728f8cde57ca4e64adeba22845fd54b13d6233a
Alex <alexandr.rusanov1@gmail.com>
2021-11-11 19:08:03 +0300

    Initial commit

---
+++ src/test/java/accountant/HomeAccountant/HomeAccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package accountant.HomeAccountant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
