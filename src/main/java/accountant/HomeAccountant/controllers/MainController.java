b4c36b612ddd4183370291a9c9108fdb9ea88021
Alex <alexandr.rusanov1@gmail.com>
2021-11-14 18:25:15 +0300

    Google charts

--- src/main/java/accountant/HomeAccountant/controllers/MainController.java
+++ src/main/java/accountant/HomeAccountant/controllers/MainController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 @Controller
 public class MainController {
     @GetMapping("/")
@@ -11,5 +14,16 @@ public class MainController {
         model.addAttribute("title", "Home page");
         model.addAttribute("name", "Guest");
         return "home";
+    }
+
+    @GetMapping("/google-charts")
+    public String googleChart(Model model) {
+        Map<String, Integer> graphData = new TreeMap<>();
+        graphData.put("2016", 147);
+        graphData.put("2017", 1256);
+        graphData.put("2018", 3856);
+        graphData.put("2019", 19807);
+        model.addAttribute("chartData", graphData);
+        return "google-charts";
     }
 }
